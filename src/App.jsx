b65e7507a2e9b42bdb696ca66c41df2a811f4948
Simranjeet20 <simranjeetkaur7902@gmail.com>
2024-03-11 15:10:11 +0530

    11-3-24 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Dashboard from './Dashboard'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import VedioPlay from './VideoPlay'
+import VideoPlay from './VideoPlay'
 
 export default function App() {
   return (
